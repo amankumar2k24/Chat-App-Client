c4d09a05db5a8fc55407871e1fbdbbd6cc16af7d
Aman Kumar <aman.kumar2k15@gmail.com>
2024-09-29 13:48:24 +0530

    adding leading and socket io  url

--- src/components/messages/Message.jsx
+++ src/components/messages/Message.jsx
@@ -25,7 +25,7 @@ const Message = ({ message }) => {
         </div>
       </div>
       <div
-        className={`px-2 py-2 rounded-lg flex justify-center items-center md:chat-bubble text-[13px] leading-3 md:text-[16px] text-white ${bubbleBgColor} ${shakeClass}`}
+        className={`px-2 py-2 rounded-lg flex justify-center items-center md:chat-bubble text-[13px] leading-3 md:leading-5 md:text-[16px] text-white ${bubbleBgColor} ${shakeClass}`}
       >
         {message?.message}
       </div>
