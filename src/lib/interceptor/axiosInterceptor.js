f8535e07d68abc275ca4da2694548adbc2126cf4
Aman Kumar <aman.kumar2k15@gmail.com>
2024-09-29 17:46:51 +0530

    Update axiosInterceptor.js

--- src/lib/interceptor/axiosInterceptor.js
+++ src/lib/interceptor/axiosInterceptor.js
@@ -8,37 +8,4 @@ const axiosInterceptor = axios.create({
   withCredentials: true, // Include cookies with requests
 });
 
-// Request interceptor for API calls
-axiosInterceptor.defaults.headers.common["Content-Type"] = "application/json";
-
-// Add a request interceptor
-axiosInterceptor.interceptors.request.use(
-  (config) => {
-    const token = document.cookie
-      .split("; ")
-      .find((row) => row.startsWith("jwt="));
-    if (token) {
-      config.headers["Authorization"] = `Bearer ${token.split("=")[1]}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-axiosInterceptor.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    if (error.response) {
-      if (error.response.status === 401) {
-        console.error("Unauthorized access. Please log in.");
-      }
-    } else {
-      console.error("An error occurred:", error.message);
-    }
-    return Promise.reject(error);
-  }
-);
-
 export default axiosInterceptor;
